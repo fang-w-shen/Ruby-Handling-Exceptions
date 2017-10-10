2c1c04b924c8e054fd6bf83262a306af4b4686df
Fang <fang-wen.shen@vanderbilt.edu>
2017-10-10 12:10:16 -0500

    Fixed Exceptions

--- main.rb
+++ main.rb
@@ -3,9 +3,21 @@ require_relative 'patient'
 new_patient = Patient.new("Jisie", "David")
 puts new_patient.first_name
 puts new_patient.last_name
-puts new_patient.patient_contact_info
-puts new_patient.patient_status("coma")
-puts new_patient.patient_currently_admitted?("11:01 A.M.")
+
+begin
+	puts patient_contact_info
+rescue NameError => exception
+	puts "No Method Error: #{exception}"
+else
+end
+puts new_patient.patient_status(:coma)
+
+begin
+	puts new_patient.patient_currently_admitted?
+rescue ArgumentError=>e
+	puts "Argument Error: #{e.message}"
+else
+end
 puts new_patient.patient_new_total(125)
-puts new_patient.add_med_to_list("lipitor", 123)
+new_patient.add_med_to_list("lipitor", 123.1)
 new_patient.patient_medications_list
--- patient.rb
+++ patient.rb
@@ -5,7 +5,7 @@ class MedTypeError < StandardError
 end
 
 class Patient
-  attr_accessor :first_name, :last_name, :status, :current_balance, :med_list,:patient_contact_info
+  attr_accessor :first_name, :last_name, :status, :current_balance, :med_list
 
   def initialize(first_name, last_name)
     @first_name = first_name
@@ -14,33 +14,37 @@ class Patient
   end
 
   def patient_status(status)
-    raise ArgumentError,"Argument is not a string" unless status.is_a? String
-    @status = status
-    puts "Status: #{@status}"
+    begin
+      raise ArgumentError,"Argument is not a string" unless status.is_a? String
+    rescue ArgumentError=>e
+      puts "Patient Status Error: #{e.message}"
+    else
+      @status = status
+    end
   end
-  def patient_contact_info
-    puts "First Name: #{@first_name}. Last Name: #{@last_name}"
-  end
+
   def patient_currently_admitted?(admit_time)
     puts "Patient currently in hospital, admitted at #{admit_time}"
   end
 
   def patient_new_total(amount)
-    @current_balance = 0
-    puts "Current balance before calculation: $#{current_balance}"
-    raise ArgumentError, "Error: Amount entered was 0" unless amount != 0
-    # @current_balance = (current_balance + amount)/current_balance
+    begin
+      @current_balance = 0
+      puts "Current balance before calculation: #{current_balance}"
+      raise ZeroDivisionError, "Can't Divide By 0"
+    rescue ZeroDivisionError =>e
+      puts "ZeroDivisionError: #{e}"
+    else
+      @current_balance = (current_balance + amount)/current_balance
+      # This runs only if there are no exceptions
+      @current_balance = amount
+    end
 
-    # This runs only if there are no exceptions
-  rescue ArgumentError=>e
-    puts e.message
-  else
-    @current_balance = amount
-    puts "Current balance after calculation: $#{current_balance}"
+
   end
 
   def add_med_to_list(med, dosage)
-
+    begin
       raise ArgumentError,"#{med} is not a string" unless med.is_a? String
 
       raise ArgumentError,"#{dosage} is not a integer" unless dosage.is_a? Integer
@@ -48,26 +52,31 @@ class Patient
       puts "Medication added: #{med}"
       @med_list.push([med,dosage])
 
-    #Create a new class called MedTypeError that inherits from StandardError and use that as seen below:
-    begin
-      raise MedTypeError, "This is not a valid medication" #unless @med_list!=nil
+      raise MedTypeError, "This is not a valid medication"
+      #Create a new class called MedTypeError that inherits from StandardError and use that as seen below:
+
+    rescue ArgumentError => e
+
+      puts "Med List Argument Error: #{e}"
     rescue MedTypeError =>e
-      @med_list.each {|v|puts "#{v[0]}: #{v[1]}mgs"}
-      puts e.message
+      puts "Med List Argument Error: #{e}"
+    else
+      puts @med_list
     end
   end
 
   def patient_medications_list
-    puts "User medication list:"
     begin
-      raise RuntimeError, "Medications list is not valid!" #unless @med_list!=nil
-    rescue RuntimeError=>e
-      puts e.message
+      puts "User medication list:"
+      raise RuntimeError, "Medications list is not valid."
+    rescue RuntimeError => e
+      puts "RuntimeError: #{e}"
     else
       puts @med_list
     ensure
-    # This needs to run whether there was an exception or not.
-    puts "Disclaimer: The hospital is not responsible for the user's health due to medications they may be using."
+      # This needs to run whether there was an exception or not.
+      puts "Disclaimer: The hospital is not responsible for the user's health due to medications they may be using."
+
+    end
   end
-end
 end
